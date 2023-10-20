f647dbedc71171748c9872d4a5350bf057928e4f
Andre Herman <amelco.herman@gmail.com>
2023-10-20 18:14:11 -0400

    feat: adiciona struct botao

--- player.c
+++ player.c
@@ -14,15 +14,13 @@
 #define BUTTON_TEXT_COLOR  DARKGRAY
 #define BUTTON_MARGIN      5
 
-// TODO: criar structs de um bnotao e de uma lista de botoes
-//       para desenhar todos de uma vez na tela através de 
-//       apenas uma função.
-//typedef struct
-//{
-//    Vector2 pos;
-//    const char* text;
-//} Button;
-//
+typedef struct
+{
+    Rectangle rectangle;
+    Color color;
+    const char* text;
+} Button;
+
 //typedef struct
 //{
 //    Button* list;
@@ -31,27 +29,18 @@
 //
 //} Button_List;
 
-void draw_button(Vector2 pos, const char* text, Color btnCollor)
+void draw_button(Button* botao)
 {
-    int textSize = MeasureText(text, BUTTON_TEXT_SIZE);
-    DrawRectangle(pos.x, pos.y, textSize + 2*BUTTON_MARGIN, BUTTON_TEXT_SIZE + 2*BUTTON_MARGIN, btnCollor);
-    DrawText(text, pos.x+BUTTON_MARGIN, pos.y+BUTTON_MARGIN, BUTTON_TEXT_SIZE, BUTTON_TEXT_COLOR);
+    int textSize = MeasureText(botao->text, BUTTON_TEXT_SIZE);
+    botao->rectangle.width = textSize + 2*BUTTON_MARGIN;
+    botao->rectangle.height = BUTTON_TEXT_SIZE + 2*BUTTON_MARGIN;
+    DrawRectangleRec(botao->rectangle, botao->color);
+    DrawText(botao->text, botao->rectangle.x+BUTTON_MARGIN, botao->rectangle.y+BUTTON_MARGIN, BUTTON_TEXT_SIZE, BUTTON_TEXT_COLOR);
 }
-
-// TODO: função para obter Rectangle de um botao
-//       para user na detecção de colisão corretamente
-//Rectangle get_button_rec(Button botao)
-//{
-//
-//}
-
 
 int main(void)
 {
     // Inicializando
-    Rectangle botao_abrir = { 10, 10, 50, 20 };
-    Color botao_abrir_cor = BACKGROUND_COLOR;
-
     InitAudioDevice();
     SetMasterVolume(0.2);
     Music music = LoadMusicStream("./music.mp3");
@@ -60,21 +49,25 @@ int main(void)
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
     SetTargetFPS(60);
 
+    // Definindo componentes gráficos da janela
+    Button botao_abrir_arquivo = {{ 10, 10, 50, 20 }, BUTTON_COLOR, "Abrir arquivo"};
+    Button botao_sair= {{ 750, 550, 50, 20 }, BUTTON_COLOR, "Sair"};
+
+    // main loop
     while (!WindowShouldClose())
     {
         // ------- Atualizando -------------
         UpdateMusicStream(music);
         Vector2 mouse_pos = GetMousePosition();
 
-        botao_abrir_cor = BUTTON_COLOR;
-        // TODO: pegar o Rectangle correto através da função get_button_rec()
-        if(CheckCollisionPointRec(mouse_pos, botao_abrir))
+        botao_abrir_arquivo.color = BUTTON_COLOR;
+        if(CheckCollisionPointRec(mouse_pos, botao_abrir_arquivo.rectangle))
         {
-            botao_abrir_cor = BUTTON_HOVER_COLOR;
+            botao_abrir_arquivo.color = BUTTON_HOVER_COLOR;
             if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
             {
                 // TODO: abrir janela com caixa de texto para digitar path do arquivo de musica
-                botao_abrir_cor = BLUE;
+                botao_abrir_arquivo.color = BLUE;
             }
         }
 
@@ -83,11 +76,8 @@ int main(void)
         BeginDrawing();
         {
             ClearBackground(BLACK);
-            
-            // DrawRectangleRec(botao_abrir, botao_abrir_cor);
-            draw_button(CLITERAL(Vector2){botao_abrir.x, botao_abrir.y}, "Abrir arquivo", botao_abrir_cor);
-
-
+            draw_button(&botao_abrir_arquivo);
+            draw_button(&botao_sair);
             if(IsKeyDown(KEY_Q))
                 break;
         }
