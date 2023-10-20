38d22257c31516e7013110eea30135385f035d79
Andre Herman <amelco.herman@gmail.com>
2023-10-20 18:27:07 -0400

    feat: cria função de verificação de mouse hover em botão

--- player.c
+++ player.c
@@ -38,39 +38,54 @@ void draw_button(Button* botao)
     DrawText(botao->text, botao->rectangle.x+BUTTON_MARGIN, botao->rectangle.y+BUTTON_MARGIN, BUTTON_TEXT_SIZE, BUTTON_TEXT_COLOR);
 }
 
+bool isButtonHovered(Button* botao)
+{
+    botao->color = BUTTON_COLOR;
+    Vector2 mouse_pos = GetMousePosition();
+    bool isHovered = CheckCollisionPointRec(mouse_pos, botao->rectangle);
+    if(isHovered)
+    {
+        botao->color = BUTTON_HOVER_COLOR;
+    }
+    return isHovered;
+}
+
 int main(void)
 {
     // Inicializando
     InitAudioDevice();
     SetMasterVolume(0.2);
-    Music music = LoadMusicStream("./music.mp3");
-    PlayMusicStream(music);
+    Music musica = LoadMusicStream("./music.mp3");
+    PlayMusicStream(musica);
 
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
     SetTargetFPS(60);
 
     // Definindo componentes gráficos da janela
     Button botao_abrir_arquivo = {{ 10, 10, 50, 20 }, BUTTON_COLOR, "Abrir arquivo"};
-    Button botao_sair= {{ 750, 550, 50, 20 }, BUTTON_COLOR, "Sair"};
+    Button botao_sair= {{ 750, 560, 50, 20 }, BUTTON_COLOR, "Sair"};
 
     // main loop
     while (!WindowShouldClose())
     {
         // ------- Atualizando -------------
-        UpdateMusicStream(music);
-        Vector2 mouse_pos = GetMousePosition();
-
-        botao_abrir_arquivo.color = BUTTON_COLOR;
-        if(CheckCollisionPointRec(mouse_pos, botao_abrir_arquivo.rectangle))
+        UpdateMusicStream(musica);
+        if (isButtonHovered(&botao_abrir_arquivo))
         {
-            botao_abrir_arquivo.color = BUTTON_HOVER_COLOR;
-            if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
+            
+            if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
             {
-                // TODO: abrir janela com caixa de texto para digitar path do arquivo de musica
-                botao_abrir_arquivo.color = BLUE;
+                // TODO: abrir janela de seleção de arquivo de musica
             }
         }
-
+        if (isButtonHovered(&botao_sair))
+        {
+            if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+            {
+                // Sai do main loop
+                break;
+            }
+        }
 
         // ------- Desenhando -------------
         BeginDrawing();
